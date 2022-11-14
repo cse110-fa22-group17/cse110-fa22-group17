110509a5d003e80b28986d0967a3974d4b7c93ba
Raon Choi <h1choi@ucsd.edu>
2022-11-13 17:53:58 -0800

    Create assets folder

---
+++ assets/scripts/sidebar.js
@@ -0,0 +1,5 @@
+/* functions for sidebar menu open/close */
+function toggleSide() {
+  document.getElementById("mySidebar").classList.toggle("active");
+  document.getElementById("main").classList.toggle("active");
+}
