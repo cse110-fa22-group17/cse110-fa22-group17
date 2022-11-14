5143bfa4eb2cfac4f85e79b46dbb397bab8a458f
h1choi0918 <78620080+h1choi0918@users.noreply.github.com>
2022-11-13 18:04:59 -0800

    Delete sidebar.js

--- assets/scripts/sidebar.js
+++
@@ -1,5 +0,0 @@
-/* functions for sidebar menu open/close */
-function toggleSide() {
-  document.getElementById("mySidebar").classList.toggle("active");
-  document.getElementById("main").classList.toggle("active");
-}
